a2d0f8ba54a7471bb6ab803684b7f0d147f109e7
JoshHyde9 <joshhyde546@gmail.com>
2022-10-24 17:07:35 +1100

    feat: added component for showing books

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
 import PropTypes from "prop-types";
 
+import { BookCard } from "./components/BookCard/BookCard";
+
 import styles from "./App.module.scss";
+import { Loader } from "./components/Loader/Loader";
 
 export const App = ({ books }) => {
+  const { data, loading } = books;
+
+  if (!data && !loading) {
+    return <p>Try searching for a book.</p>;
+  }
+
+  if (loading && !data) {
+    return <Loader width="100px" hexColor="ff00ff" />;
+  }
+
   return (
-    <div>
-      <h1>Hello World!</h1>
-      {JSON.stringify(books)}
+    <div className={styles.gallery}>
+      {data.map((book, i) => (
+        <BookCard key={i} volumeInfo={book.volumeInfo} />
+      ))}
     </div>
   );
 };
 
 App.propTypes = {
   books: PropTypes.shape({
-    books: PropTypes.object,
+    data: PropTypes.array,
     loading: PropTypes.bool,
   }),
 };
