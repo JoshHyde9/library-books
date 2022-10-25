bf0a5588bc30f2ae96104ada6ea2610dfcbfb8d1
JoshHyde9 <joshhyde546@gmail.com>
2022-10-25 15:43:17 +1100

    feat: added loader

--- src/components/pages/Book.jsx
+++ src/components/pages/Book.jsx
@@ -1,16 +1,16 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-/**
- *
- * @param {book: {title: string}} props
- */
+import { Loader } from "../Loader/Loader";
+
 export const Book = () => {
   const { bookId } = useParams();
 
+  const [loading, setLoading] = useState(false);
   const [book, setBook] = useState({});
 
   useEffect(() => {
+    setLoading(true);
     const getBook = async () => {
       const response = await fetch(
         `https://www.googleapis.com/books/v1/volumes/${bookId}`
@@ -18,9 +18,15 @@ export const Book = () => {
 
       const data = await response.json();
       setBook(data.volumeInfo);
+      setLoading(false);
     };
     getBook();
   }, []);
+
+  if (loading) {
+    return <Loader hexColor="#ff00ff" />;
+  }
+
   return (
     <div>
       <h1>This is the book's page</h1>
